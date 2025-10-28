2b97ff5245ab02940cc5f8a2b714682b6642a0b9
ravisankar666 <ravisankarp666@gmail.com>
2025-10-28 22:51:01 +0530

    some message

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,9 +1,20 @@
-module "vote_service_sg" {
-    source = "terraform-aws-modules/security-group/aws"
+# using open source module
+# module "vote_service_sg" {
+#     source = "terraform-aws-modules/security-group/aws"
 
-    name = "${local.common_name_suffix}-catalogue"
-    description = "security group for catalogue with custom ports open within VPC,egress all traffic"
-    vpc_id = data.aws_ssm_parameter.vpc_id.value
+#     name = "${local.common_name_suffix}-catalogue"
+#     description = "security group for catalogue with custom ports open within VPC,egress all traffic"
+#     vpc_id = data.aws_ssm_parameter.vpc_id.value
 
-}    
+# }    
     
+
+    module "sg" {
+        source = "git::https://github.com/ravisankar666/terraform-aws-sg.git?ref=main"
+        project_name = var.project_name
+        environment = var.environment
+        sg_name = "mongodb"
+        sg_description = "Created for mongodb"
+        vpc_id = local.vpc_id
+      
+    }
