f539d4a7eb1ad84f84d7b86b6e6152a970bab409
ravisankar666 <ravisankarp666@gmail.com>
2025-10-30 23:43:48 +0530

    some message

--- 30-sg-rule/main.tf
+++ 30-sg-rule/main.tf
@@ -1,7 +1,16 @@
 resource "aws_security_group_rule" "frontend_frontend_alb" {
   type              = "ingress"
-  security_group_id = module.sg[9].sg_id # frontend SG ID
+  security_group_id = local.backend_alb_sg_id # frontend SG ID
   source_security_group_id = module.sg[11].sg_id # frontend ALB SG ID
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+}
+
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  security_group_id = local.bustion_sg_id 
+  source_security_group_id = 
   from_port         = 80
   protocol          = "tcp"
   to_port           = 80
